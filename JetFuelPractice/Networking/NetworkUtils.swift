f73c073e0bd3c30acd5813ef4716f0706f3cdad6
MarquisSimmons <msimmons0009@gmail.com>
2021-01-07 16:58:41 -0500

    Added Comments
    - Added function comments to the NetworkUtils function

--- JetFuelPractice/Networking/NetworkUtils.swift
+++ JetFuelPractice/Networking/NetworkUtils.swift
@@ -11,6 +11,14 @@ public enum NetworkUtils {
     static let DEFAULT_LINK = URL(string: "https://www.plugco.in/public/take_home_sample_feed")!
     static let DOCUMENTS_PATH = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first! as URL
     
+    
+    /// This function is responsible for retrieving an Image from a url and converting it into a UIImage
+    /// - Parameters:
+    ///   - imageUrl: The URL of the Image we want to display
+    ///   - completion: The completion handler
+    ///   - image: The UIImage retrieved or nil if there was an error
+    ///   - error: The error returned or nil if we retrieved the image successfully
+    
     static func getImageFromURL(imageUrl: URL, completion: @escaping (_ image: UIImage?, _ error: String?) -> Void) {
         URLSession.shared.dataTask(with: imageUrl) { (data, response, error) in
             if let error = error {
@@ -24,6 +32,14 @@ public enum NetworkUtils {
         }.resume()
         
     }
+    
+    
+    
+    /// This function is responsible for retrieving an Image from a url and converting it into a UIImage
+    /// - Parameters:
+    ///   - completion: The completion handler
+    ///   - campaignItems: The Object representation of the items in the feed
+    ///   - error: The error returned or nil if we retrieved the feed json successfully
     
     static func getFeed(completion: @escaping (_ campaignItems: [CampaignItem]?, _ error: String?) -> Void) {
         URLSession.shared.dataTask(with: DEFAULT_LINK) { (data, response, error) in
@@ -68,6 +84,11 @@ public enum NetworkUtils {
         }.resume()
     }
     
+    
+    /// This function is responsible for downloading media from the feed into the user's Photo Library
+    /// - Parameters:
+    ///   - contentUrl: The url of the media we want to download
+    ///   - contentType: The content type of the media we want to download. It can be  a video or a photo
     static func downloadContent(contentUrl: URL, contentType: String){
         let fileExtension = contentType == "video" ? ".mov" : ".jpg"
         let filePath = DOCUMENTS_PATH.appendingPathComponent(String(format: "JetFuelMedia%d" + fileExtension, arc4random() % 1000))
@@ -86,7 +107,7 @@ public enum NetworkUtils {
                                 if completed {
                                     print("Video asset created")
                                 } else {
-                                    print(error)
+                                    print(error?.localizedDescription)
                                 }
                             }
                         }
